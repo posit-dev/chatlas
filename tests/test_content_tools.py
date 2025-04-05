5e7aa6c89d21c7b78e54862263dcb84b64869875
Carson <cpsievert1@gmail.com>
2025-04-04 19:38:50 -0500

    Add tests for a custom result class

--- tests/test_content_tools.py
+++ tests/test_content_tools.py
@@ -1,9 +1,8 @@
-from typing import Union, Any, Optional
+from typing import Any, Optional, Union
 
 import pytest
-
 from chatlas import ChatOpenAI
-from chatlas.types import ContentToolResult, ContentToolRequest
+from chatlas.types import ContentToolRequest, ContentToolResult
 
 
 def test_register_tool():
@@ -105,11 +104,11 @@ def test_invoke_tool_returns_tool_result():
 
     def tool():
         return 1
-    
+
     chat.register_tool(tool)
 
     def new_tool_request(
-        name: Optional[str] = "tool", 
+        name: Optional[str] = "tool",
         args: Optional[dict[str, Any]] = None,
     ):
         return ContentToolRequest(
@@ -131,10 +130,12 @@ def test_invoke_tool_returns_tool_result():
     assert "got an unexpected keyword argument" in str(res.error)
     assert res.value is None
 
-    res = chat._invoke_tool(new_tool_request(
-        name="foo",
-        args={"x": 1},
-    ))
+    res = chat._invoke_tool(
+        new_tool_request(
+            name="foo",
+            args={"x": 1},
+        )
+    )
     assert isinstance(res, ContentToolResult)
     assert res.id == "id"
     assert str(res.error) == "Unknown tool: foo"
@@ -147,7 +148,7 @@ async def test_invoke_tool_returns_tool_result_async():
 
     async def tool():
         return 1
-    
+
     chat.register_tool(tool)
 
     def new_tool_request(
@@ -166,19 +167,113 @@ async def test_invoke_tool_returns_tool_result_async():
     assert res.error is None
     assert res.value == 1
 
-    res = await chat._invoke_tool_async(
-        new_tool_request(args={"x": 1})
-    )
+    res = await chat._invoke_tool_async(new_tool_request(args={"x": 1}))
     assert isinstance(res, ContentToolResult)
     assert res.id == "id"
     assert res.error is not None
     assert "got an unexpected keyword argument" in str(res.error)
     assert res.value is None
 
-    res = await chat._invoke_tool_async(
-        new_tool_request(name="foo", args={"x": 1})
-    )
+    res = await chat._invoke_tool_async(new_tool_request(name="foo", args={"x": 1}))
     assert isinstance(res, ContentToolResult)
     assert res.id == "id"
     assert str(res.error) == "Unknown tool: foo"
     assert res.value is None
+
+
+def test_tool_custom_result():
+    chat = ChatOpenAI()
+
+    class CustomResult(ContentToolResult):
+        pass
+
+    def custom_tool():
+        return CustomResult(value=1, extra={"foo": "bar"})
+
+    def custom_tool_err():
+        return CustomResult(
+            value=None,
+            error=Exception("foo"),
+            extra={"foo": "bar"},
+        )
+
+    chat.register_tool(custom_tool)
+    chat.register_tool(custom_tool_err)
+
+    req = ContentToolRequest(
+        id="id",
+        name="custom_tool",
+        arguments={},
+    )
+    req_err = ContentToolRequest(
+        id="id",
+        name="custom_tool_err",
+        arguments={},
+    )
+
+    res = chat._invoke_tool(req)
+    assert isinstance(res, CustomResult)
+    assert res.id == "id"
+    assert res.error is None
+    assert res.value == 1
+    assert res.extra == {"foo": "bar"}
+    assert res.request == req
+
+    res_err = chat._invoke_tool(req_err)
+    assert isinstance(res_err, CustomResult)
+    assert res_err.id == "id"
+    assert res_err.error is not None
+    assert str(res_err.error) == "foo"
+    assert res_err.value is None
+    assert res_err.extra == {"foo": "bar"}
+    assert res_err.request == req_err
+
+
+@pytest.mark.asyncio
+async def test_tool_custom_result_async():
+    chat = ChatOpenAI()
+
+    class CustomResult(ContentToolResult):
+        pass
+
+    async def custom_tool():
+        return CustomResult(value=1, extra={"foo": "bar"})
+
+    async def custom_tool_err():
+        return CustomResult(
+            value=None,
+            error=Exception("foo"),
+            extra={"foo": "bar"},
+        )
+
+    chat.register_tool(custom_tool)
+    chat.register_tool(custom_tool_err)
+
+    req = ContentToolRequest(
+        id="id",
+        name="custom_tool",
+        arguments={},
+    )
+
+    req_err = ContentToolRequest(
+        id="id",
+        name="custom_tool_err",
+        arguments={},
+    )
+
+    res = await chat._invoke_tool_async(req)
+    assert isinstance(res, CustomResult)
+    assert res.id == "id"
+    assert res.error is None
+    assert res.value == 1
+    assert res.extra == {"foo": "bar"}
+    assert res.request == req
+
+    res_err = await chat._invoke_tool_async(req_err)
+    assert isinstance(res_err, CustomResult)
+    assert res_err.id == "id"
+    assert res_err.error is not None
+    assert str(res_err.error) == "foo"
+    assert res_err.value is None
+    assert res_err.extra == {"foo": "bar"}
+    assert res_err.request == req_err
