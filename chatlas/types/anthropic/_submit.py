17e88f5dc45da00ed0a16f8f47d7a33a040cc6c8
Carson <cpsievert1@gmail.com>
2025-05-22 21:14:32 -0500

    Update types

--- chatlas/types/anthropic/_submit.py
+++ chatlas/types/anthropic/_submit.py
@@ -29,9 +29,15 @@ class SubmitInputArgs(TypedDict, total=False):
             "claude-3-7-sonnet-20250219",
             "claude-3-5-haiku-latest",
             "claude-3-5-haiku-20241022",
+            "claude-sonnet-4-20250514",
+            "claude-sonnet-4-0",
+            "claude-4-sonnet-20250514",
             "claude-3-5-sonnet-latest",
             "claude-3-5-sonnet-20241022",
             "claude-3-5-sonnet-20240620",
+            "claude-opus-4-0",
+            "claude-opus-4-20250514",
+            "claude-4-opus-20250514",
             "claude-3-opus-latest",
             "claude-3-opus-20240229",
             "claude-3-sonnet-20240229",
@@ -41,6 +47,7 @@ class SubmitInputArgs(TypedDict, total=False):
         ],
         str,
     ]
+    service_tier: Union[Literal["auto", "standard_only"], anthropic.NotGiven]
     stop_sequences: Union[list[str], anthropic.NotGiven]
     stream: Union[Literal[False], Literal[True], anthropic.NotGiven]
     system: Union[
