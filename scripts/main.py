04bf10ccd2216618e77d1bf9c2627f1a7439732d
Carson <cpsievert1@gmail.com>
2025-05-29 16:15:12 -0500

    chore: Resurrect torch install hack; cleanup type generation

--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,6 @@ scripts = [
     "_generate_anthropic_types.py",
     "_generate_google_types.py",
     "_generate_openai_types.py",
-    "_generate_snowflake_types.py",
 ]
 
 absolute_scripts = [Path(__file__).parent / script for script in scripts]
