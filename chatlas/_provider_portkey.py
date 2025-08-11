1fc1209bd4cf3af263bc85a82b8630e353cef7d9
Carson <cpsievert1@gmail.com>
2025-08-11 15:05:33 -0500

    add to doctsring; add to API reference

--- chatlas/_provider_portkey.py
+++ chatlas/_provider_portkey.py
@@ -29,6 +29,60 @@ def ChatPortkey(
     provides an interface (AI Gateway) to connect through its Universal API to a
     variety of LLMs providers with a single endpoint.
 
+    Prerequisites
+    -------------
+
+    ::: {.callout-note}
+    ## Portkey credentials
+
+    Follow the instructions at <https://portkey.ai/docs/introduction/make-your-first-request>
+    to get started making requests to PortkeyAI. You will need to set the
+    `PORTKEY_API_KEY` environment variable to your Portkey API key, and optionally
+    the `PORTKEY_VIRTUAL_KEY` environment variable to your virtual key.
+    :::
+
+    Examples
+    --------
+    ```python
+    import os
+    from chatlas import ChatPortkey
+
+    chat = ChatPortkey(api_key=os.getenv("PORTKEY_API_KEY"))
+    chat.chat("What is the capital of France?")
+    ```
+
+    Parameters
+    ----------
+    system_prompt
+        A system prompt to set the behavior of the assistant.
+    model
+        The model to use for the chat. The default, None, will pick a reasonable
+        default, and warn you about it. We strongly recommend explicitly
+        choosing a model for all but the most casual use.
+    api_key
+        The API key to use for authentication. You generally should not supply
+        this directly, but instead set the `PORTKEY_API_KEY` environment variable.
+    virtual_key
+        An (optional) virtual identifier, storing the LLM provider's API key. See
+        [documentation](https://portkey.ai/docs/product/ai-gateway/virtual-keys).
+        You generally should not supply this directly, but instead set the
+        `PORTKEY_VIRTUAL_KEY` environment variable.
+    base_url
+        The base URL for the Portkey API. The default is suitable for most users.
+    kwargs
+        Additional arguments to pass to the OpenAIProvider, such as headers or
+        other client configuration options.
+
+    Returns
+    -------
+    Chat
+        A chat object that retains the state of the conversation.
+
+    Notes
+    -----
+    This function is a lightweight wrapper around [](`~chatlas.ChatOpenAI`) with
+    the defaults tweaked for PortkeyAI.
+
     """
     if model is None:
         model = log_model_default("gpt-4.1")
